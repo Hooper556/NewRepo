e2b05934d69f44c377ca8083cdec63c427e57c86
Hooper556 <wilson.3371@buckeyemail.osu.edu>
2023-10-07 20:02:21 -0400

    commit

--- pages/desktop.js
+++ pages/desktop.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "./topNav";
-import ReactPlayer from "react-player";
 
 export default function Desktop() {
   return (
