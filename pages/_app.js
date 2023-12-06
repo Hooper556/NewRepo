0507169ead0d5d5c2a6c7afd64e9ef4a5a645508
vince <vincemillerce@gmail.com>
2023-12-06 12:08:53 -0600

    give the page a head for seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
-import "../styles/globals.css";
+import "../styles/globals.css"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  function renderHead() {
+    return (
+      <Head>
+        <title>
+          {"Keen Betting: Unlock Smart Betting Potential - Master Arbitrage for Maximum Wins"}
+        </title>
+        <meta
+          property="og:title"
+          content="Keen Betting: Unlock Smart Betting Potential - Master Arbitrage for Maximum Wins"
+          key="og-title"
+        />
+        <meta
+          property="og:description"
+          content="Leveraging 'Mathematical Advantage' to Generate Massive Growth in Sports Betting Profits"
+          key="og-description"
+        />
+        <meta
+          property="twitter:description"
+          content="Leveraging 'Mathematical Advantage' to Generate Massive Growth in Sports Betting Profits"
+          key="twitter-description"
+        />
+        <meta
+          name="description"
+          content="Leveraging 'Mathematical Advantage' to Generate Massive Growth in Sports Betting Profits"
+        />
+        <meta property="og:image" content="https://keenbetting.com/background3.jpg" key="image" />
+        <meta property="og:url" content="https://keenbetting.com" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:site" content="@KeenBetting" />
+      </Head>
+    )
+  }
+
+  return (
+    <>
+      {renderHead()}
+      <Component {...pageProps} />;
+    </>
+  )
 }
 
-export default MyApp;
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,31 @@
-import { useEffect, useState } from "react";
-import Loader from "./loader";
-import Desktop from "./desktop";
-import Mobile from "./mobile";
+import { useEffect, useState } from "react"
+import Loader from "./loader"
+import Desktop from "./desktop"
+import Mobile from "./mobile"
 
 export default function Home() {
-  const [isBusy, setIsBusy] = useState(true);
-  const [width, setWidth] = useState(10);
-  const [height, setHeight] = useState(10);
+  const [isBusy, setIsBusy] = useState(true)
+  const [width, setWidth] = useState(10)
+  const [height, setHeight] = useState(10)
 
-  var viewportWidth = null;
-  var viewportHeight = null;
+  var viewportWidth = null
+  var viewportHeight = null
 
   useEffect(() => {
-    viewportWidth = window.innerWidth;
-    viewportHeight = window.innerHeight;
-    setWidth(viewportWidth);
-    setHeight(viewportHeight);
-    setIsBusy(false);
-  }, []);
+    viewportWidth = window.innerWidth
+    viewportHeight = window.innerHeight
+    setWidth(viewportWidth)
+    setHeight(viewportHeight)
+    setIsBusy(false)
+  }, [])
 
   if (isBusy === true) {
-    return <Loader />;
+    return <Loader />
   } else {
     if (width >= 600) {
-      return <Desktop />;
+      return <Desktop />
     } else {
-      return <Mobile width={width} height={height} />;
+      return <Mobile width={width} height={height} />
     }
   }
 }
