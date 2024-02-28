ecfdde0d4474f9b548648d240a1bed8dd573e26a
vince <vincemillerce@gmail.com>
2024-02-28 10:48:24 -0600

    fix redirect for google indexing

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,14 +3,14 @@ import getPosts from "./blog/data"
 export default async function sitemap() {
   const posts = await getPosts()
   const blogs = posts.map((post) => ({
-    url: `https://keenbetting.com/blog/${post.slug}`,
+    url: `https://www.keenbetting.com/blog/${post.slug}`,
     lastModified: post.lastModified
       ? new Date(post.lastModified).toISOString().split("T")[0]
       : new Date().toISOString().split("T")[0],
   }))
 
   const routes = ["", "/blog"].map((route) => ({
-    url: `https://keenbetting.com${route}`,
+    url: `https://www.keenbetting.com${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }))
 
