687a098acf6f539af8ada0007e8b9c339c1fd64c
vince <vincemillerce@gmail.com>
2024-02-28 10:59:25 -0600

    update twitter url

--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -1,7 +1,7 @@
 export const SENDY_URL = "http://ec2-3-16-169-201.us-east-2.compute.amazonaws.com/sendy"
 export const LANDING_PAGE_WAITLIST = ""
 export const EMAIL = "KeenTechnology1@gmail.com"
-export const TWITTER_URL = "https://twitter.com/KeenBetting"
+export const TWITTER_URL = "https://twitter.com/Keen_Betting"
 export const INSTAGRAM_URL = "https://www.instagram.com/keenbetting/"
 export const GOOGLE_PLAY_URL =
   "https://play.google.com/store/apps/details?id=com.gladiatorfitnessinc.trial1&pcampaignid=web_share&pli=1"
