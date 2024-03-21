83f06bb215408593ce64e1c5b3b3697a8be88db8
vince <vincemillerce@gmail.com>
2024-03-20 19:54:08 -0500

    add unfurl route

---
+++ src/app/[sport]/[eventId]/page.tsx
@@ -0,0 +1,70 @@
+
+import type { Metadata } from 'next';
+import { UNFURL_API } from "../../constants";
+import Image from "next/image";
+import DownloadBadge from "../../home/download-badge";
+import BackgroundImage from "../../../../public/bball.webp";
+
+type Props = {
+  params: { sport: string, eventId: string; };
+};
+
+const sportMap = { 'NCAAB': 'basketball_ncaab' };
+
+export async function generateMetadata(
+  { params }: Props,
+): Promise<Metadata> {
+
+  const sport = sportMap[params.sport] || params.sport;
+
+  return {
+    title: params.sport,
+    openGraph: {
+      images: [
+        {
+          url: `${UNFURL_API}/keen/odds-shop?sport=${sport}&eventId=${params.eventId}`,
+          width: 1200,
+          height: 630,
+          alt: "Keen Betting",
+        },
+      ],
+    },
+  };
+}
+
+export default function EventPage({
+  params,
+}: {
+  params: {
+    sport: string;
+    eventId: string;
+  };
+}) {
+  return <section className="h-screen w-full relative">
+    <Image
+      src={BackgroundImage}
+      alt="basketball stadium"
+      className="object-cover"
+      priority
+      fill
+    />
+
+    <div className="absolute inset-0 flex-col items-center justify-center z-10">
+      <div className="flex flex-col gap-12 items-center justify-center h-full w-full">
+        <div className="text-white flex flex-col gap-8 text-center justify-center items-center font-semibold">
+          <div className="transition ease-in-out duration-1000 hover:scale-110">
+            <h1 className="text-5xl md:text-8xl">Join the betting revolution. 💰</h1>
+          </div>
+          <h2 className=" text-white md:text-3xl font-semibold px-4 md:px-8 w-full md:w-2/3">
+            {
+              "Leveraging 'mathematical advantage' to generate massive growth in sports betting profits"
+            }
+          </h2>
+        </div>
+        <div className="flex flex-col md:flex-row items-center h-[67px] min-h-[67px]">
+          <DownloadBadge />
+        </div>
+      </div>
+    </div>
+  </section>;
+}
--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -1,9 +1,10 @@
-export const SENDY_URL = "http://ec2-3-16-169-201.us-east-2.compute.amazonaws.com/sendy"
-export const LANDING_PAGE_WAITLIST = ""
-export const EMAIL = "KeenTechnology1@gmail.com"
-export const TWITTER_URL = "https://twitter.com/Keen_Betting"
-export const INSTAGRAM_URL = "https://www.instagram.com/keenbetting/"
+export const SENDY_URL = "http://ec2-3-16-169-201.us-east-2.compute.amazonaws.com/sendy";
+export const LANDING_PAGE_WAITLIST = "";
+export const EMAIL = "KeenTechnology1@gmail.com";
+export const TWITTER_URL = "https://twitter.com/Keen_Betting";
+export const INSTAGRAM_URL = "https://www.instagram.com/keenbetting/";
+export const UNFURL_API = "https://unfurl-api.obsurv.app";
 export const GOOGLE_PLAY_URL =
-  "https://play.google.com/store/apps/details?id=com.gladiatorfitnessinc.trial1&pcampaignid=web_share&pli=1"
+  "https://play.google.com/store/apps/details?id=com.gladiatorfitnessinc.trial1&pcampaignid=web_share&pli=1";
 export const APPLE_APP_STORE_URL =
-  "https://apps.apple.com/us/app/keen-betting-arbitrage-ev/id6451397700"
+  "https://apps.apple.com/us/app/keen-betting-arbitrage-ev/id6451397700";
